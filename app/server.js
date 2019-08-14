9b8b283922ab3834e55b7b279143bb05d3ecd962
jugs <jageshmaharjan@hotmail.com>
2019-08-14 19:25:27 +0800

    db.js added

--- app/server.js
+++ app/server.js
@@ -19,6 +19,7 @@ console.log(`Running on http://${HOST}:${PORT}`);
 const express = require('express');
 const app = express();
 const router = express.Router();
+const db = require('./db');
 
 const path = __dirname + '/views/';
 const port = 8080;
@@ -28,18 +29,20 @@ router.use(function (req,res,next) {
   next();
 });
 
-router.get('/', function(req,res){
+router.get('/',function(req,res){
   res.sendFile(path + 'index.html');
 });
 
-router.get('/sharks', function(req,res){
+router.get('/sharks',function(req,res){
   res.sendFile(path + 'sharks.html');
 });
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path));
 app.use('/', router);
 
 app.listen(port, function () {
   console.log('Example app listening on port 8080!')
 })
-
